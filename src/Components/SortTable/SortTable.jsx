448d85bb9fe50ce5c9c3f13cb1a66b173cd2e343
sophie mattacks <s.mattacks@hotmail.co.uk>
2023-04-05 11:35:58 +0100

    All Athlete info in table

--- src/Components/SortTable/SortTable.jsx
+++ src/Components/SortTable/SortTable.jsx
@@ -1,7 +1,6 @@
 import React, {useState} from 'react';
 import marathonResults from '../../Data/MarathonResults.json';
 
-
 function SortTable() {
   const [athletes, setAthletes] = useState(marathonResults.results.athletes);
 
@@ -9,18 +8,33 @@ function SortTable() {
     <div>
       <table>
         <thead>
-          <th>First Name</th>
-          <th>Surname</th>
-          <th>Athlete ID</th>
-          <th>Finish Time</th>
+          <tr>
+            <th>Rank</th>
+            <th>First Name</th>
+            <th>Surname</th>
+            <th>Athlete ID</th>
+            <th>Finish Time</th>
+            <th>Race Progress</th>
+            <th>Team Name</th>
+            <th>Bib Number</th>
+            <th>Flag</th>
+            <th>Country</th>
+          </tr>
         </thead>
 
         <tbody>
           { athletes.map((athlete)=>(
-            <tr key={athlete.id}>
+            <tr key={athlete.rank}>
               <td>{athlete.rank}</td>
               <td>{athlete.firstname}</td>
               <td>{athlete.surname}</td>
+              <td>{athlete.athleteid}</td>
+              <td>{athlete.finishtime}</td>
+              <td>{athlete.raceprogress}</td>
+              <td>{athlete.teamname}</td>
+              <td>{athlete.bibnumber}</td>
+              <td>{athlete.flag}</td>
+              <td>{athlete.countryname}</td>
             </tr>
           ))}
 
